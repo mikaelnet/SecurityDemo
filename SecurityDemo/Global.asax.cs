e062eeb69f22cffec4e47d36c6930342d68c5b40
mikael.hogberg <mikael.hogberg@stendahls.se>
2016-03-11 22:16:27 +0100

    First commit

---
+++ SecurityDemo/Global.asax.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace SecurityDemo
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Remove "X-AspNetMvc-Version" header
+            MvcHandler.DisableMvcResponseHeader = true;
+        }
+
+        protected void Application_PreSendRequestHeaders()
+        {
+            // Remove "Microsoft-IIS/10.0" header
+            Response.Headers.Remove("Server");
+        }
+    }
+}
