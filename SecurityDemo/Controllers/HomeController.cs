14b47f162faec6c4e6278d9f294af8341c577bd1
mikael.hogberg <mikael.hogberg@stendahls.se>
2016-03-13 12:57:26 +0100

    staging

--- SecurityDemo/Controllers/HomeController.cs
+++ SecurityDemo/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +10,22 @@ namespace SecurityDemo.Controllers
 {
     public class HomeController : Controller
     {
+        //public static readonly HtmlString ShaScript = new HtmlString("alert('Cacheable content using sha512');");
+
         public ActionResult Index()
         {
+            //var scriptHash = GetSha256Hash(ShaScript.ToString());
+            //Response.Headers.Add("Content-Security-Policy", "default-src 'self'; script-src 'self' '"+scriptHash+"'");
             return View();
         }
+
+        /*public static string GetSha256Hash(string text)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            var hashString = new SHA512Managed();
+            byte[] hash = hashString.ComputeHash(bytes);
+            return "sha512-"+Convert.ToBase64String(hash);
+        }*/
 
         public ActionResult About()
         {
